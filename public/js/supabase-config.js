1baa6cbb79e915cece8dc0cad0b6598167ef9bc8
DeVechtseKoeriers <devechtsekoeriers@gmail.com>
2026-02-01 16:31:45 +0200

    Update supabase-config.js
    Fix supabase config syntax

--- public/js/supabase-config.js
+++ public/js/supabase-config.js
@@ -1,4 +1,4 @@
-/ Supabase config (public, veilig voor frontend)
+// Supabase config (public, veilig voor frontend)
 const SUPABASE_URL = "https://jvgdeymaqmuoaexkhdda.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2Z2RleW1xcW11b2FleGtoZHRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5Mjk0NTksImV4cCI6MjA4NTUwNTQ1OX0.sN86Xgzc2l_vevR8SWU9KJ7Q6HTKZxrUp1bTyCNNkII";
 
