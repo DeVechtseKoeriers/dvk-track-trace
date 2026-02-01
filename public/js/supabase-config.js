eca06824e220117e28cd7b4c3aa0d70cfbe5b681
DeVechtseKoeriers <devechtsekoeriers@gmail.com>
2026-02-01 11:27:51 +0200

    Create supabase-config.js
    Add supabase config

---
+++ public/js/supabase-config.js
@@ -0,0 +1,3 @@
+// Vul dit in vanuit Supabase -> Settings -> API
+window.DVK_SUPABASE_URL = "https://gvgdeymqqmuoaexkhdta.supabase.co";
+window.DVK_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2Z2RleW1xcW11b2FleGtoZHRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5Mjk0NTksImV4cCI6MjA4NTUwNTQ1OX0.sN86Xgzc2l_vevR8SWU9KJ7Q6HTKZxrUp1bTyCNNkII";
