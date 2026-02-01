578c3c49311caadbaac7dd6f114f7c74c72ec440
DeVechtseKoeriers <devechtsekoeriers@gmail.com>
2026-02-01 18:38:41 +0200

    Update supabase-config.js

--- public/js/supabase-config.js
+++ public/js/supabase-config.js
@@ -2,8 +2,5 @@
 const SUPABASE_URL = "https://gvgdeymqqmuoaexkhdta.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2Z2RleW1xcW11b2FleGtoZHRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5Mjk0NTksImV4cCI6MjA4NTUwNTQ1OX0.sN86Xgzc2l_vevR8SWU9KJ7Q6HTKZxrUp1bTyCNNkII";
 
-// Maak 1 globale client
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+// 1 globale client
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
