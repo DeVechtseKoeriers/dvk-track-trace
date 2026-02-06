68dcadb94caec3f5522115b93e4c6c34848b7ef4
DeVechtseKoeriers <devechtsekoeriers@gmail.com>
2026-02-06 12:39:34 +0200

    Update track.js

--- public/js/track.js
+++ public/js/track.js
@@ -17,8 +17,8 @@
     TRACK: "track_code",
     STATUS: "status",
     CUSTOMER: "customer_name",
-    COLLI: "colli",
-    KG: "kg", // <- als jij 'weight_kg' hebt: zet dit op "weight_kg"
+    COLLI: "colli_count",
+    KG: "weight_kg", // <- als jij 'weight_kg' hebt: zet dit op "weight_kg"
     DESC: "package_desc", // als anders: bv "omschrijving"
     ADDRESS: "customer_address",
     PHONE: "customer_phone",
